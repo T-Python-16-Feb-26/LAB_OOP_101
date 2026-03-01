7a6992666ed2d4e742bdc9d16a7b84cb67d1a583
Wajn <wajnaq1@gmail.com>
2026-03-01 14:34:44 +0300

    done.

---
+++ main.py
@@ -0,0 +1,19 @@
+from panda import Panda
+panda1 = Panda("Luna", 3, 100, "Black & White")
+panda2 = Panda("Mili", 5, 121, "Black & White")
+panda3 = Panda("sesa", 2, 93, "Black & White")
+panda4 = Panda("Leo", 4, 106, "Black & White")
+
+print(panda1.name)
+
+panda1.eat()
+panda1.sleep()
+
+panda2.eat()
+panda2.sleep()
+
+panda3.eat()
+panda3.sleep()
+
+panda4.eat()
+panda4.sleep()
